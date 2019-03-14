57ce9a04b291b54969e7dbbfe19a91b1ebd0d312
glennton <ricebananas@gmail.com>
2019-03-14 02:03:02 -0700

    Update template with more usage examples

--- src/client/components/Template/index.jsx
+++ src/client/components/Template/index.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
-import { Col } from 'react-materialize'
-import './styles.scss'
+import PropTypes from 'prop-types'
+import Grid from '@material-ui/core/Grid'
+import { withStyles } from '@material-ui/core/styles';
 
-class Component extends React.Component {
+const styles = theme => ({
+  CONTAINER:{
+    [theme.breakpoints.up('xs')]: { //0
+    },
+    [theme.breakpoints.up('sm')]: { //600
+    },
+    [theme.breakpoints.up('md')]: { //960
+    },
+    [theme.breakpoints.up('lg')]: { //1280
+    },
+    [theme.breakpoints.up('xl')]: { //1920
+    },
+  }
+})
+class CONTAINER extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    this.sampleFunction = this.sampleFunction.bind(this)
+  }
+
+  sampleFunction(x){
+    return x
+  }
   render() {
     return (
-      <div className="container">
-        <Col s={12}>
-          <p>App</p>
-        </Col>
-      </div>
+      <Grid className={`${this.props.classes.CONTAINER}`}>
+      </Grid>
     );
   }
+  componentDidMount() {
+    this.setState({
+      //
+    })
+  }
 }
-  
-export default Component
-  
+
+CONTAINER.propTypes = {
+  classes: PropTypes.object,
+}
+
+export default withStyles(styles)(CONTAINER);
